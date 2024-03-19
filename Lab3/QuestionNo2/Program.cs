4507ed9e581e054bf7c27e4c8cf3006aac690f8f
SishirDangi <shishirdangi541@gmail.com>
2024-03-19 15:33:52 +0545

    Lab 3

---
+++ Lab3/QuestionNo2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputString = "Hello World";
+
+        var uppercaseLetters = inputString.Where(char.IsUpper);
+
+        Console.WriteLine("Uppercase letters in the string:");
+        foreach (var letter in uppercaseLetters)
+        {
+            Console.WriteLine(letter);
+        }
+    }
+}
---
+++ Lab3/QuestionNo3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Input array
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+        // LINQ query to find the numbers and their squares
+        var numbersAndSquares = from num in numbers
+                                select new { Number = num, Square = num * num };
+
+        // Output the numbers and their squares
+        Console.WriteLine("Number\tSquare");
+        foreach (var item in numbersAndSquares)
+        {
+            Console.WriteLine($"{item.Number}\t{item.Square}");
+        }
+    }
+}
